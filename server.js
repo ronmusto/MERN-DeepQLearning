24451e4f880fa0b2ef875c648dd2abd99faedcf8
ronmusto <83174514+ronmusto@users.noreply.github.com>
2023-07-20 22:49:30 -0400

    improved security
    moved login credentials out of local storage and hashed passwords upon registration as well as other minor fixes related to login persistence issues

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const cookieParser = require('cookie-parser');
+
 let fetch;
 import('node-fetch').then(nodeFetch => {
   fetch = nodeFetch;
@@ -13,9 +17,13 @@ const mongoURI = 'mongodb://localhost:27017/MERN-DeepQLearning';
 const secret = 'secret-key';
 
 app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
+  origin: 'http://localhost:3000',  // replace with the origin of your client
+  methods: ['GET', 'POST'],  // add other HTTP methods if needed
+  credentials: true  // this enables cookies to be sent with requests
 }));
+
+
+app.use(cookieParser());
 
 app.use(express.json());
 
@@ -66,39 +74,43 @@ MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true 
           res.status(500).json({ error: 'Failed to retrieve users' });
         });
     });
-
+    
     app.post('/register', (req, res) => {
-      const newUser = { 
-        email: req.body.email, 
-        password: req.body.password 
-      };
-      console.log('New user:', newUser);
-      db.collection('users')
-        .insertOne(newUser)
-        .then(result => {
-          console.log('Insert result:', result); 
-          // Fetch the document that was just inserted
-          return db.collection('users').findOne({ _id: result.insertedId });
-        })
-        .then(user => {
-          // Send the user document back in the response
-          res.json({ user });
-        })
-        .catch(err => {
-          console.error('Error registering user:', err);
-          res.status(500).json({ error: 'Failed to register user', message: err.message });
-        });
+      const { email, password } = req.body;
+      bcrypt.hash(password, saltRounds, function(err, hash) {
+        const newUser = { email, password: hash };
+        console.log('New user:', newUser);
+        db.collection('users')
+          .insertOne(newUser)
+          .then(result => {
+            console.log('Insert result:', result); 
+            return db.collection('users').findOne({ _id: result.insertedId });
+          })
+          .then(user => {
+            res.json({ user });
+          })
+          .catch(err => {
+            console.error('Error registering user:', err);
+            res.status(500).json({ error: 'Failed to register user', message: err.message });
+          });
+      });
     });
-
+    
     app.post('/login', (req, res) => {
       const { email, password } = req.body;
       db.collection('users')
-        .findOne({ email, password })
+        .findOne({ email })
         .then(user => {
           if (user) {
-            // Create a JWT and return it in the response
-            const token = jwt.sign({ _id: user._id }, secret);
-            res.json({ user, token }); // Include the user object in the response
+            bcrypt.compare(password, user.password, function(err, result) {
+              if(result) {
+                const token = jwt.sign({ _id: user._id }, secret);
+                res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'Strict' });  // set cookie here
+                res.json({ user }); 
+              } else {
+                res.status(401).json({ error: 'Invalid login credentials' });
+              }
+            });
           } else {
             res.status(401).json({ error: 'Invalid login credentials' });
           }
@@ -108,21 +120,22 @@ MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true 
           res.status(500).json({ error: 'Failed to login' });
         });
     });
-
-    app.post('/verify', (req, res) => {
-      const token = req.headers.authorization.split(' ')[1];
+    
+    
+    app.get('/verify', (req, res) => {
+      const token = req.cookies.token;  // read the token from the cookie
+      console.log('Token:', token);  // log the token
       jwt.verify(token, secret, (err, decoded) => {
         if (err) {
+          console.error('Token verification error:', err);
           return res.status(401).json({ error: 'Invalid token' });
         }
-    
-        // Convert _id string to ObjectId
         const ObjectId = require('mongodb').ObjectId;
         const _id = new ObjectId(decoded._id);
-        
         db.collection('users')
-          .findOne({ _id })  // Use ObjectId in query
+          .findOne({ _id })
           .then((user) => {
+            console.log('User:', user);  // log the user
             if (user) {
               res.json({ user });
             } else {
