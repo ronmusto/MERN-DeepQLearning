889e848d20d453277a9dd63adf589b009326a10e
ronmusto <83174514+ronmusto@users.noreply.github.com>
2023-08-14 23:20:13 -0400

    added endpoints for account page
    retrieve user details and user bookings

--- server.js
+++ server.js
@@ -103,6 +103,46 @@ MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true 
         });
     });
 
+    app.get('/user-data/:userId', (req, res) => {
+      const userId = req.params.userId;
+  
+      db.collection('users')
+          .findOne({ _id: ObjectId(userId) })
+          .then(user => {
+              if (!user) {
+                  return res.status(404).json({ error: 'User not found' });
+              }
+              res.json({
+                  email: user.email
+              });
+          })
+          .catch(err => {
+              console.error('Error retrieving user data:', err);
+              res.status(500).json({ error: 'Failed to retrieve user data' });
+          });
+    });
+
+    app.get('/user-booked-vacations/:userId', (req, res) => {
+      const userId = req.params.userId;
+  
+      db.collection('User-Bookings')
+          .find({ userId: ObjectId(userId) })
+          .toArray()
+          .then(bookings => {
+              const vacationIds = bookings.map(booking => ObjectId(booking.vacationId));
+              return db.collection('User-Bookings')
+                  .find({ _id: { $in: vacationIds } })
+                  .toArray();
+          })
+          .then(vacations => {
+              res.json(vacations);
+          })
+          .catch(err => {
+              console.error('Error retrieving booked vacations:', err);
+              res.status(500).json({ error: 'Failed to retrieve booked vacations' });
+          });
+    });  
+
     // Endpoint for aggregated data based on timeframe 2009-2010
     app.get('/retail-data-2009-2010-aggregated-timeframe', (req, res) => {
       const limit = parseInt(req.query.limit) || 100; // default limit to 100 documents
